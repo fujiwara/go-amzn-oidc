e4bb9c51c4719aba6aebc3c8dfdebe04aa1cc994
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2022-07-22 10:02:58 +0900

    validate if token is expired

--- validator/claims.go
+++ validator/claims.go
@@ -1,7 +1,17 @@
 package validator
 
+import (
+	"github.com/pkg/errors"
+	"time"
+)
+
 type Claims map[string]interface{}
 
 func (c *Claims) Valid() error {
+	now := time.Now()
+	exp := time.Unix(c.Exp(), 0)
+	if !now.Before(exp) {
+		return errors.New("token is expired")
+	}
 	return nil
 }
